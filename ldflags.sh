851edd223ec8fb0dfe942825f3fed64d55058d7d
Paddy <paddy@impractical.co>
2016-07-08 10:51:19 -0700

    Fix package declaration.
    Whooops my shell scripting is embarrassing. :(

--- ldflags.sh
+++ ldflags.sh
@@ -2,12 +2,11 @@ githash=$(git rev-parse --short HEAD)
 gittag=$(git name-rev --tags --name-only $githash)
 gitbranch=$(git rev-parse --abbrev-ref HEAD)
 now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-package="${PACKAGE}"
 VERSION=${VERSION:-$githash}
 TAG=${TAG:-$gittag}
 BRANCH=${BRANCH:-$branch}
 TIMESTAMP=${TIMESTAMP:-$now}
-PACKAGE=$package
+PACKAGE=darlinggo.co/version
 if [ ! -z $PACKAGE_PREFIX ]; then
 	PACKAGE="${PACKAGE_PREFIX}/vendor/${PACKAGE}"
 fi
